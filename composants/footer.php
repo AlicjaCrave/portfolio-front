f35e6c906992e78b24b21cf2a8fd4d64fd5a8284
AlicjaCrave <aliciacrave66@gmail.com>
2021-04-21 15:59:15 +0200

    css

--- composants/footer.php
+++ composants/footer.php
@@ -1,9 +1,9 @@
 <footer>
         <nav id="bottom">
             <div id="social">
-                <a href="www.facebook.com"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                <a href="twitter.com"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                <a href="instagram.com"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="https://www.facebook.com" ><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                <a href="https://twitter.com"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                <a href="https://instagram.com"><i class="fa fa-instagram" aria-hidden="true"></i></a>
             </div> 
             <div id="droits"><i class="fa fa-copyright" aria-hidden="true"></i>
  2021 | Pierre-Henri Crave</div>  
